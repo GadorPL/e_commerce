59ec42dba0af8b7a0cf85008e1520499c3c51ca5
Michał Hewelt <mhewelt9@gmail.com>
2022-11-05 15:51:31 +0100

    add index view

--- shop/models.py
+++ shop/models.py
@@ -9,3 +9,6 @@ class Product(models.Model):
     category = models.CharField(max_length=200)
     description = models.TextField()
     image = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title
