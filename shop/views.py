7c9141c7a9159b9849453c5d93074bafaa5caafe
Michał Hewelt <mhewelt9@gmail.com>
2022-11-07 16:49:52 +0100

    add pagination

--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from .models import Product
+from django.core.paginator import Paginator
 
 
 # Create your views here.
 def index(request):
     product_objects = Product.objects.all()
 
+    # search
     item_name = request.GET.get('item_name')
     if item_name != '' and item_name is not None:
         product_objects = product_objects.filter(title__icontains=item_name)
+
+    # pagination
+    paginator = Paginator(product_objects, 4)
+    page = request.GET.get('page')
+    product_objects = paginator.get_page(page)
+
     return render(request, 'shop/index.html', {'product_objects': product_objects})
