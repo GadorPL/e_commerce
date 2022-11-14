1db14fa0d130a7b6e95175a72ac205efbf27c14f
Michał Hewelt <mhewelt9@gmail.com>
2022-11-14 21:40:45 +0100

    add checkout form, add order model

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Order
 from django.core.paginator import Paginator
 
 
@@ -26,4 +26,15 @@ def detail(request, id):
 
 
 def checkout(request):
+    if request.method == "POST":
+        name = request.POST.get("name", "")
+        email = request.POST.get("email", "")
+        address = request.POST.get("address", "")
+        city = request.POST.get("city", "")
+        state = request.POST.get("state", "")
+        zipcode = request.POST.get("zipcode", "")
+
+        order = Order(name=name, email=email, address=address, city=city, state=state, zipcode=zipcode)
+        order.save()
+
     return render(request, 'shop/checkout.html')
